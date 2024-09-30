775ca0e09350462db693ed1a34a1506a19b12c3c
Marco Ho <mho122@my.bcit.ca>
2024-09-29 22:50:09 -0700

    Remove redundant comments

--- COMP4537/labs/3/PartA/app.js
+++ COMP4537/labs/3/PartA/app.js
@@ -1,5 +1,3 @@
-// const mo = require('./modules/math');
-// console.log(`Hello Marco.  This the sum of 6 and 4 is ${mo.add(6, 4)}`)
 
 const mo = require('./modules/math');
 console.log(`Hello Marco.  This the sum of 6 and 4 is ${mo.add(6, 4)}`)
