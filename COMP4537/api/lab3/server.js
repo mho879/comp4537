435c18313db8d50806abfa4efd2a2565059e7389
Marco Ho <mho122@my.bcit.ca>
2024-09-30 02:18:03 -0700

    Update

--- COMP4537/api/lab3/server.js
+++ COMP4537/api/lab3/server.js
@@ -23,7 +23,7 @@ class Server {
     constructor(language) {
         this.MessageHandler = new MessageDisplay(language);
         this.date = new DateTimeRetriever();
-        // this.server = http.createServer(this.handleRequest.bind(this));
+        this.server = http.createServer(this.handleRequest.bind(this));
     }
 
     /**
@@ -62,49 +62,6 @@ module.exports = (req, res) => {
     server.handleRequest(req, res);
 };
 
-// /**
-//  * Main class
-//  * Drives the server
-//  */
-// class main {
-//     constructor(port, language) {
-//         this.server = new Server(port, language);
-//         this.server.start();
-//     }
-// }
+const myServer = new Server('en');
+myServer.start();
 
-// new main(8000, 'en')
-
-// const http = require('node:http');
-// const { URL } = require('node:url');
-// const { MessageDisplay, DateTimeRetriever } = require('./modules/util.js'); // Adjusted to relative path
-// const BLUE_TEXT = '\x1b[34m';
-
-// /**
-//  * Handles incoming HTTP requests
-//  * 
-//  * @param {*} req received request object
-//  * @param {*} res response object to return
-//  */
-// const handleRequest = (req, res) => {
-//     const url = new URL(req.url, `https://${req.headers.host}`); // Use 'https' for Vercel
-//     const params = new URLSearchParams(url.search);
-//     const name = params.get('name');
-//     const message = new MessageDisplay('en').getFunctionMessage('greetingMessage', [name, new DateTimeRetriever().getCurrentDateTime()]);
-
-//     res.writeHead(200, {
-//         'Content-Type': 'text/html; charset=utf-8',
-//     });
-
-//     res.end(BLUE_TEXT + message);
-// };
-
-// // Vercel serverless function handler
-// module.exports = (req, res) => {
-//     if (req.method === 'GET') {
-//         handleRequest(req, res);
-//     } else {
-//         res.writeHead(405, { 'Content-Type': 'text/plain' });
-//         res.end('Method Not Allowed');
-//     }
-// };
