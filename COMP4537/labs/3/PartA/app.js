8c89ee005ed28f26dff6ccf74390831226a25c2c
Marco Ho <mho122@my.bcit.ca>
2024-09-29 22:49:33 -0700

    Fix part A code

--- COMP4537/labs/3/PartA/app.js
+++ COMP4537/labs/3/PartA/app.js
@@ -1,2 +1,5 @@
+// const mo = require('./modules/math');
+// console.log(`Hello Marco.  This the sum of 6 and 4 is ${mo.add(6, 4)}`)
+
 const mo = require('./modules/math');
 console.log(`Hello Marco.  This the sum of 6 and 4 is ${mo.add(6, 4)}`)
--- COMP4537/labs/3/PartA/modules/math.js
+++ COMP4537/labs/3/PartA/modules/math.js
@@ -1,8 +1,8 @@
 function add(numOne, numTwo) {
     return numOne + numTwo;
 }
-
 function subtract(numOne, numTwo) {
     return numOne - numTwo;
 }
 
+module.exports = {add, subtract};
