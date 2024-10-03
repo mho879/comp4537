45cc8fa451bf997fd5bb5839bd062cf644f1a99f
Marco Ho <mho122@my.bcit.ca>
2024-10-02 18:45:45 -0700

    Remove submodules and test files

--- COMP4537/api/lab3/lang/messages/en/user.js
+++
@@ -1,4 +0,0 @@
-const MESSAGES = {
-    greetingMessage: (args) => `Hello ${args[0]}.  What a beautiful day.  Server current date and time is ${args[1]}.`
-}
-module.exports = MESSAGES;
--- COMP4537/labs/5/frontend/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text'});
-    res.end('Hello <b>World!</b>');
-}).listen(5000);
-
-console.log('Server is running and listening');
